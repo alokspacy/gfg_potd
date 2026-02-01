0ad4fa4b126d73d7a73c683eab98012a94eff486
Alok Singh <193059476+alokspacy@users.noreply.github.com>
2026-02-01 23:32:34 +0530

    Add K Sized Subarray Maximum solution
    Implement a solution to find maximums of all k-sized subarrays.

---
+++ Day11.java
@@ -0,0 +1,28 @@
+//K Sized Subarray Maximum
+
+import java.util.*;
+
+class Solution {
+    public ArrayList<Integer> maxOfSubarrays(int[] arr, int k) {
+        ArrayList<Integer> result = new ArrayList<>();
+        Deque<Integer> dq = new LinkedList<>();
+        
+        for (int i = 0; i < arr.length; i++) {
+            while (!dq.isEmpty() && dq.peekFirst() <= i - k) {
+                dq.pollFirst();
+            }
+            
+            while (!dq.isEmpty() && arr[dq.peekLast()] <= arr[i]) {
+                dq.pollLast();
+            }
+          
+            dq.offerLast(i);
+            
+            if (i >= k - 1) {
+                result.add(arr[dq.peekFirst()]);
+            }
+        }
+        
+        return result;
+    }
+}
