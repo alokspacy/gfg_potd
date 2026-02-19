45fdaa94686c279cc1b2b9ef0b374177e446eca9
Alok Singh <193059476+alokspacy@users.noreply.github.com>
2026-02-19 19:46:28 +0530

    Implement missingRange method to find missing numbers

---
+++ Day29.java
@@ -0,0 +1,22 @@
+// Missing Element in Range
+
+import java.util.*;
+
+class Solution {
+    public ArrayList<Integer> missingRange(int[] arr, int low, int high) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : arr) {
+            set.add(num);
+        }
+        
+        ArrayList<Integer> result = new ArrayList<>();
+      
+        for (int i = low; i <= high; i++) {
+            if (!set.contains(i)) {
+                result.add(i);
+            }
+        }
+        
+        return result;
+    }
+}
