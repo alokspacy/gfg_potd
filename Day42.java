57c0e1af6576689e2dba9b2721a1caaee961531e
Alok Singh <193059476+alokspacy@users.noreply.github.com>
2026-03-04 08:35:30 +0530

    Implement maxSubarrayXOR method in Solution class

---
+++ Day42.java
@@ -0,0 +1,22 @@
+// Max Xor Subarray of size K
+
+class Solution {
+    public int maxSubarrayXOR(int[] arr, int k) {
+        int n = arr.length;
+        
+        int currXor = 0;
+        for (int i = 0; i < k; i++) {
+            currXor ^= arr[i];
+        }
+        
+        int maxXor = currXor;
+
+        for (int i = k; i < n; i++) {
+            currXor ^= arr[i - k]; 
+            currXor ^= arr[i];     
+            maxXor = Math.max(maxXor, currXor);
+        }
+        
+        return maxXor;
+    }
+}
