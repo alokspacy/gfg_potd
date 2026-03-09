5f575e4570aa01da9f31233c5065b4015e6bbe20
Alok Singh <193059476+alokspacy@users.noreply.github.com>
2026-03-09 11:53:41 +0530

    Add largestSwap method to find largest number in swap
    Implement largestSwap method to swap digits for largest number.

---
+++ Day47.java
@@ -0,0 +1,27 @@
+// Largest number in one swap
+
+class Solution {
+    public String largestSwap(String s) {
+        char[] arr = s.toCharArray();
+        int n = arr.length;
+
+        int[] lastPos = new int[10];
+        for (int i = 0; i < n; i++) {
+            lastPos[arr[i] - '0'] = i;
+        }
+
+        for (int i = 0; i < n; i++) {
+            int current = arr[i] - '0';
+            // Check digits larger than current
+            for (int d = 9; d > current; d--) {
+                if (lastPos[d] > i) {
+                    char temp = arr[i];
+                    arr[i] = arr[lastPos[d]];
+                    arr[lastPos[d]] = temp;
+                    return new String(arr);
+                }
+            }
+        }
+        return s;
+    }
+}
