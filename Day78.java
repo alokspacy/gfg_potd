cb419bcf78e0eef3bb67d662963bd95a979dc6c4
Alok Singh <193059476+alokspacy@users.noreply.github.com>
2026-04-09 09:32:05 +0530

    Implement intersection of two sorted arrays

---
+++ Day78.java
@@ -0,0 +1,26 @@
+// Intersection of Two Sorted Arrays
+
+import java.util.*;
+
+class Solution {
+    ArrayList<Integer> intersection(int[] a, int[] b) {
+        ArrayList<Integer> res = new ArrayList<>();
+        int i = 0, j = 0;
+        
+        while (i < a.length && j < b.length) {
+            if (a[i] == b[j]) {
+                // Add only distinct elements
+                if (res.isEmpty() || res.get(res.size() - 1) != a[i]) {
+                    res.add(a[i]);
+                }
+                i++;
+                j++;
+            } else if (a[i] < b[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        return res;
+    }
+}
