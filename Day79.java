0f538104dc09587a85645860428e1d98885265bd
Alok Singh <193059476+alokspacy@users.noreply.github.com>
2026-04-10 07:28:27 +0530

    Implement method to find sorted subsequence of size 3

---
+++ Day79.java
@@ -0,0 +1,46 @@
+// Sorted subsequence of size 3
+
+import java.util.*;
+
+class Solution {
+    public ArrayList<Integer> find3Numbers(int[] arr) {
+        int n = arr.length;
+        ArrayList<Integer> res = new ArrayList<>();
+        if (n < 3) return res;
+
+        int[] smaller = new int[n];
+        int[] greater = new int[n];
+
+        smaller[0] = -1;
+        int minIndex = 0;
+        for (int i = 1; i < n; i++) {
+            if (arr[i] <= arr[minIndex]) {
+                minIndex = i;
+                smaller[i] = -1;
+            } else {
+                smaller[i] = minIndex;
+            }
+        }
+
+        greater[n - 1] = -1;
+        int maxIndex = n - 1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] >= arr[maxIndex]) {
+                maxIndex = i;
+                greater[i] = -1;
+            } else {
+                greater[i] = maxIndex;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (smaller[i] != -1 && greater[i] != -1) {
+                res.add(arr[smaller[i]]);
+                res.add(arr[i]);
+                res.add(arr[greater[i]]);
+                return res;
+            }
+        }
+        return res;
+    }
+}
