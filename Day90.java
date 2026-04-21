8449f66c19ede97c451f18b0aad9b3c783039a29
Alok Singh <193059476+alokspacy@users.noreply.github.com>
2026-04-21 09:41:14 +0530

    Implement solution for two water jug problem

---
+++ Day90.java
@@ -0,0 +1,37 @@
+// Two water Jug problem
+
+class Solution {
+    public int minSteps(int m, int n, int d) {
+        if (d > Math.max(m, n)) return -1;
+        if (d % gcd(m, n) != 0) return -1;
+        
+        return Math.min(pour(m, n, d), pour(n, m, d));
+    }
+
+    private int pour(int fromCap, int toCap, int d) {
+        int from = fromCap, to = 0, step = 1;
+        
+        while (from != d && to != d) {
+            int temp = Math.min(from, toCap - to);
+            to += temp;
+            from -= temp;
+            step++;
+            
+            if (from == d || to == d) break;
+            
+            if (from == 0) {
+                from = fromCap;
+                step++;
+            }
+            if (to == toCap) {
+                to = 0;
+                step++;
+            }
+        }
+        return step;
+    }
+
+    private int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+}
