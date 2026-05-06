b1e4b1eaee2c8cfad66311e481b0c8b19a7a9586
Alok Singh <193059476+alokspacy@users.noreply.github.com>
2026-05-06 08:08:18 +0530

    Implement method to get size of binary tree

---
+++ Day105.java
@@ -0,0 +1,8 @@
+//Size of Binary Tree
+
+class Solution {
+    public int getSize(Node root) {
+        if (root == null) return 0;
+        return 1 + getSize(root.left) + getSize(root.right);
+    }
+}
