ea90c08265e729535e91d265948b0e6cf4354d07
Alok Singh <193059476+alokspacy@users.noreply.github.com>
2026-06-23 09:53:41 +0530

    Implement maxPeopleDefeated method in Solution class

---
+++ Day152.java
@@ -0,0 +1,16 @@
+// Maximum Number of People Defeated
+
+class Solution {
+int maxPeopleDefeated(int p) {
+// code here
+int i = 1;
+int count = 0;
+while (p >= (i*i))
+{
+       p = p - (i*i);
+       count++;
+       i++;
+}
+return count;
+}
+};
