a9a9e46714810810d8100c81b37fbbdca1a5905c
Alok Singh <193059476+alokspacy@users.noreply.github.com>
2026-07-05 09:54:52 +0530

    Implement maxCharGap method to find max gaps

---
+++ Day164.java
@@ -0,0 +1,25 @@
+// Max Gap Between Two Same
+
+import java.util.Arrays;
+
+class Solution {
+    public int maxCharGap(String s) {
+        int[] firstIndex = new int[26];
+        Arrays.fill(firstIndex, -1);
+        
+        int maxGap = -1;
+        
+        for (int i = 0; i < s.length(); i++) {
+            int charIdx = s.charAt(i) - 'a';
+            
+            if (firstIndex[charIdx] != -1) {
+                int currentGap = i - firstIndex[charIdx] - 1;
+                maxGap = Math.max(maxGap, currentGap);
+            } else {
+                firstIndex[charIdx] = i;
+            }
+        }
+        
+        return maxGap;
+    }
+}
