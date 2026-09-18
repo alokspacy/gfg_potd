b92d6fde572ecd9c42154cf3407845567044392d
Alok Singh <193059476+alokspacy@users.noreply.github.com>
2026-09-18 10:00:46 +0530

    Create Day239.java

---
+++ Day239.java
@@ -0,0 +1,29 @@
+// Minimum Absolute Difference In BST
+
+class Solution {
+    int prev = -1;
+    int minDiff = Integer.MAX_VALUE;
+
+    public int absDiff(Node root) {
+        inorder(root);
+        return minDiff;
+    }
+
+    private void inorder(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        // Left
+        inorder(root.left);
+
+        // Current
+        if (prev != -1) {
+            minDiff = Math.min(minDiff, root.data - prev);
+        }
+        prev = root.data;
+
+        // Right
+        inorder(root.right);
+    }
+}
